2e30d1aecbaee8acb6bb10f6fbc123f1221cd9df
Aleksandra Wilk <aleksandra_wilk@yahoo.com>
2025-05-08 13:51:13 +0200

    add list & bookmark models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_08_114725) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_08_115015) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "list_id"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
