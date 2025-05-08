743c4fdd12ac448904e613dc3de637d40cd51d4a
Aleksandra Wilk <aleksandra_wilk@yahoo.com>
2025-05-08 15:24:17 +0200

    add validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,9 @@
 class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, length: { minimum: 6 }
+  validates_associated :list
+  validates_associated :movie
+  validates :movie_id, uniqueness: {scope: :list_id, message: "is already in the list"}
 end
